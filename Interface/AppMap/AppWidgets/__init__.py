720d8eb6e0b5b0dff61bab3dcb814845868ad3a2
aashish <aashish.swami2606@gmail.com>
2026-05-06 13:29:02 +0200

    Onnodige files uit RTK branch verwijderen

--- Interface/AppMap/AppWidgets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .AppFrame import AppFrame
-from .AppFrameMap import AppFrameMap
-from .AppFrameRobots import AppFrameRobots
-from .AppScrolFrameRobots import AppScrolFrameRobots
-from .Robot import Robot
-
